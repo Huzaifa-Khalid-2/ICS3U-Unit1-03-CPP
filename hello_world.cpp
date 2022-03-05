00689b3dc36e07b7bb9e071697b460d79cc422c1
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-03-05 01:05:04 +0000

    added hello world with cpp

---
+++ hello_world.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 MTHS. All rights reserved
+//
+// Created by: Huzaifa Khalid
+// Created on: March 2022
+// This is the "Hello, World!" program, with proper style
+#include <iostream>
+int main() {
+std::cout << "Hello, World!";
+}
