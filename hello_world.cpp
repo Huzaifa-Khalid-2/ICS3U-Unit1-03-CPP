482c648bbdcab5665a57138540b236e2b09cd669
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-03-06 14:11:19 +0000

    za warduo

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,9 @@
 // Created by: Huzaifa Khalid
 // Created on: March 2022
 // This is the "Hello, World!" program, with proper style
+
 #include <iostream>
+
 int main() {
-std::cout << "Hello, World!";
+    std::cout << "Hello, World!";
 }
